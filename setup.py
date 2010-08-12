17168c21dfff0382be93c2f57bbc3eae00ca8098
Joshua Tacoma <joshua@yellowseed.org>
2010-08-12 21:02:40 +0000

    version 0.1.6

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except ImportError:
 
 setup(
     name='geometriki',
-    version='0.1.5',
+    version='0.1.6',
     description='A semantic wiki or literate database encoded in the tables embedded in each page.',
     author='Joshua Tacoma',
     author_email='joshua@yellowseed.org',
